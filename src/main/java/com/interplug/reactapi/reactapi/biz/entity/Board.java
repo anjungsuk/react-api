38a0e8de8547f280ee79037a287ea3e77d3a5de2
jungukang <rusarmata@naver.com>
2024-12-20 17:22:43 +0900

    feat
    mock 컨트롤러 생성 및 log4j jdbc 연동

--- src/main/java/com/interplug/reactapi/reactapi/biz/entity/Board.java
+++ src/main/java/com/interplug/reactapi/reactapi/biz/entity/Board.java
@@ -4,16 +4,113 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 @Entity
-@Table
+@Table(name = "bt_tb_board")
 public class Board {
 
     @Id
-    @Column(name = "board_no", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer boardNo;
+    @Column(name = "board_no", nullable = false)
+    private int boardNo;
 
+    @Column(name = "category_cd", length = 30)
+    private String categoryCd;
 
+    @Column(name = "title", nullable = false, length = 200)
+    private String title;
+
+    @Column(name = "cont", columnDefinition = "TEXT")
+    private String cont;
+
+    @Column(name = "writer_nm", nullable = false, length = 50)
+    private String writerNm;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "view_cnt")
+    private Integer viewCnt;
+
+    @Column(name = "reg_dt", nullable = false)
+    private LocalDateTime regDt;
+
+    @Column(name = "mod_dt")
+    private LocalDateTime modDt;
+
+    // Getters and Setters
+    public int getBoardNo() {
+        return boardNo;
+    }
+
+    public void setBoardNo(int boardNo) {
+        this.boardNo = boardNo;
+    }
+
+    public String getCategoryCd() {
+        return categoryCd;
+    }
+
+    public void setCategoryCd(String categoryCd) {
+        this.categoryCd = categoryCd;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCont() {
+        return cont;
+    }
+
+    public void setCont(String cont) {
+        this.cont = cont;
+    }
+
+    public String getWriterNm() {
+        return writerNm;
+    }
+
+    public void setWriterNm(String writerNm) {
+        this.writerNm = writerNm;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getViewCnt() {
+        return viewCnt;
+    }
+
+    public void setViewCnt(Integer viewCnt) {
+        this.viewCnt = viewCnt;
+    }
+
+    public LocalDateTime getRegDt() {
+        return regDt;
+    }
+
+    public void setRegDt(LocalDateTime regDt) {
+        this.regDt = regDt;
+    }
+
+    public LocalDateTime getModDt() {
+        return modDt;
+    }
+
+    public void setModDt(LocalDateTime modDt) {
+        this.modDt = modDt;
+    }
 }
